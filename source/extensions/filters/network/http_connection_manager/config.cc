b8a040ce50498cbe072421c6dbf60441508a531e
Lukasz Dziedziak <lukasz.dziedziak@allegro.pl>
2019-10-24 13:22:24 +0200

    Fixed build
    Signed-off-by: Lukasz Dziedziak <lukasz.dziedziak@allegro.pl>

--- source/extensions/filters/network/http_connection_manager/config.cc
+++ source/extensions/filters/network/http_connection_manager/config.cc
@@ -10,6 +10,7 @@
 #include "envoy/server/admin.h"
 #include "envoy/tracing/http_tracer.h"
 
+#include "common/access_log/access_log_formatter.h"
 #include "common/access_log/access_log_impl.h"
 #include "common/common/fmt.h"
 #include "common/config/filter_json.h"
@@ -56,28 +57,35 @@ FilterFactoryMap::const_iterator findUpgradeCaseInsensitive(const FilterFactoryM
   return upgrade_map.end();
 }
 
+std::unordered_map<std::string, std::string> convertJsonFormatToMap(ProtobufWkt::Struct json_format) {
+  std::unordered_map<std::string, std::string> output;
+  for (const auto& pair : json_format.fields()) {
+    if (pair.second.kind_case() != ProtobufWkt::Value::kStringValue) {
+      throw EnvoyException("Only string values are supported in the JSON access log format.");
+    }
+    output.emplace(pair.first, pair.second.string_value());
+  }
+  return output;
+}
+
 AccessLog::FormatterPtr createFormatter(const Protobuf::Message& config,
                                               Server::Configuration::FactoryContext& context) {
-  const auto& format_config = MessageUtil::downcastAndValidate<const envoy::config::filter::network::v2::HttpConnectionManager&>(
+  const auto& local_reply_config = MessageUtil::downcastAndValidate<const envoy::config::filter::network::http_connection_manager::v2::LocalReplyConfig&>(
           config, context.messageValidationVisitor());
   AccessLog::FormatterPtr formatter;
 
-  // if (fal_config.access_log_format_case() == envoy::config::accesslog::v2::FileAccessLog::kFormat ||
-  //     fal_config.access_log_format_case() ==
-  //         envoy::config::accesslog::v2::FileAccessLog::ACCESS_LOG_FORMAT_NOT_SET) {
-  //   if (fal_config.format().empty()) {
-  //     formatter = AccessLog::AccessLogFormatUtils::defaultAccessLogFormatter();
-  //   } else {
-  //     formatter = std::make_unique<AccessLog::FormatterImpl>(fal_config.format());
-  //   }
-  // } else if (fal_config.access_log_format_case() ==
-  //            envoy::type::v2::StringOrJson) {
-  //   auto json_format_map = this->convertJsonFormatToMap(fal_config.json_format());
-  //   formatter = std::make_unique<AccessLog::JsonFormatterImpl>(json_format_map);
-  // } else {
-  //   throw EnvoyException(
-  //       "Invalid access_log format provided. Only 'format' and 'json_format' are supported.");
-  // }
+  if (local_reply_config.has_format()) {
+    if (local_reply_config.format().format_case() == envoy::type::StringOrJson::kStringFormat) {
+      formatter = std::make_unique<Envoy::AccessLog::FormatterImpl>(local_reply_config.format().string_format());
+    } else if (local_reply_config.format().format_case() == envoy::type::StringOrJson::kJsonFormat){
+      auto json_format_map = convertJsonFormatToMap(local_reply_config.format().json_format());
+      formatter = std::make_unique<AccessLog::JsonFormatterImpl>(json_format_map);
+    } else {
+      formatter = AccessLog::AccessLogFormatUtils::defaultAccessLogFormatter();
+    }
+  } else {
+    formatter = AccessLog::AccessLogFormatUtils::defaultAccessLogFormatter();
+  }
 
   return formatter;
 }
@@ -408,7 +416,10 @@ HttpConnectionManagerConfig::HttpConnectionManagerConfig(
   }
 
   //local_reply
-
+  if(config.has_local_reply_config()){
+    AccessLog::FormatterPtr formatter = createFormatter(config.local_reply_config(), context);
+    std::cout << "Test formatter" ;
+  }
 }
 
 void HttpConnectionManagerConfig::processFilter(
